feefab3482661aabc75d0e5518c1b051b5e1eb68
Burak Övence <ovence29@gmail.com>
2023-07-12 21:23:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRkuKQfrbNeOTgDfsuOyYrpGrXzOomD9E",
-  authDomain: "otobilet-4610c.firebaseapp.com",
-  projectId: "otobilet-4610c",
-  storageBucket: "otobilet-4610c.appspot.com",
-  messagingSenderId: "794878967198",
-  appId: "1:794878967198:web:0769ca4acaae161848aa8f",
-  measurementId: "G-W5TB9ZEJG4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
